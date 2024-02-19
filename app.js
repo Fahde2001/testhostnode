ce27913f763c58b0eefab7cb6d806755064c1e1f
Fahde2001 <fahde.othmane.it7@gmail.com>
2024-02-19 23:45:37 +0100

    version test

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+app.get('/test', (req, res) => {
+  res.send('Hello, Fahde!');
+});
+app.get('/oth', (req, res) => {
+  res.send('Hello, othamne!');
+});
+
 app.listen(8080,() => {
   console.log(`Server listening on port`);
 });
